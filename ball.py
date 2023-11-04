35c0c80d0dd27f4a5ca7b94c797a810720d7883d
xorud1210 <rhxo1210@naver.com>
2023-11-04 22:31:06 +0900

    경기장, 공 클래스 생성

---
+++ ball.py
@@ -0,0 +1,31 @@
+from pico2d import load_image, draw_rectangle
+
+import game_world
+import play_mode
+
+
+class Ball:
+    def __init__(self):
+        self.x = 800
+        self.y = 300
+        self.x_dir = 0
+        self.y_dir = 0
+        self.image = load_image('resource/stadium/ball.png')
+
+    def update(self):
+        self.x += self.x_dir * 4
+        self.y += self.y_dir * 3
+        if self.x_dir < 0:
+            self.x_dir += 0.05
+        elif self.x_dir > 0:
+            self.x_dir -= 0.05
+        if self.y_dir < 0:
+            self.y_dir += 0.05
+        elif self.y_dir > 0:
+            self.y_dir -= 0.05
+
+    def draw(self):
+        self.image.draw(self.x,self.y,64,64)
+        draw_rectangle(*self.get_bb())
+
+ 
---
+++ stadium.py
@@ -0,0 +1,48 @@
+from pico2d import *
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+class Stadium:
+    def __init__(self):
+        self.stadium = load_image('resource/stadium/topview.png')
+        # self.background = load_image('resource/stadium/backgrond.png')
+        # self.camera_pos = 0;
+        # self.camera_dir = 0;
+        self.x = 800
+        self.y = 450
+        self.width = 1600
+        self.height = 900
+
+    def update(self):
+        # self.camera_pos += self.camera_dir * 5
+        pass
+
+    def handle_event(self, event):
+        pass
+        # if event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:
+        #     self.camera_dir -= 1
+        # elif event.type == SDL_KEYUP and event.key == SDLK_LEFT:
+        #     self.camera_dir += 1
+        # elif event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
+        #     self.camera_dir += 1
+        # elif event.type == SDL_KEYUP and event.key == SDLK_RIGHT:
+        #     self.camera_dir -= 1
+
+    def draw(self):
+        # self.background.draw(800,400,1600,900)
+        self.stadium.draw(self.x,self.y,self.width,self.height)
+        draw_rectangle(*self.get_bb())
+        # self.stadium.draw(800,300)
