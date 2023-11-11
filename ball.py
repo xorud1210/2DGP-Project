d28d768e89b9761902d65c1c929f0100a46cd5fa
xorud1210 <rhxo1210@naver.com>
2023-11-11 22:39:22 +0900

    플레이어, 경기장과의 충돌 처리

--- ball.py
+++ ball.py
@@ -28,4 +28,24 @@ class Ball:
         self.image.draw(self.x,self.y,64,64)
         draw_rectangle(*self.get_bb())
 
+    def get_bb(self):
+        return self.x - 32., self.y - 32, self.x + 32, self.y + 32
 
+    def handle_collision(self, group, other):
+        if group == 'stadium:ball':
+            if self.x > 1455 :
+                self.x_dir *= -1
+                self.x = 1455
+            elif self.x < 151:
+                self.x_dir *= -1
+                self.x = 151
+            if self.y > 815:
+                self.y_dir *= -1
+                self.y = 815
+            elif self.y < 85:
+                self.y_dir *= -1
+                self.y = 85
+        if group == 'player:ball':
+            self.x_dir = (self.x - other.x) / 50
+            self.y_dir = (self.y - other.y) / 50
+            print(self.x_dir,self.y_dir)
