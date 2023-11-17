ba4befd640ba060cd0bd732517d27687fc3772a3
xorud1210 <rhxo1210@naver.com>
2023-11-18 05:25:47 +0900

    이동 거리 및 프레임 속도 단위화

--- game_framework.py
+++ game_framework.py
@@ -1,5 +1,14 @@
 import time
 
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30cm
+RUN_SPEED_KMPH = 10.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 10.0
 
 def change_mode(mode):
     global stack
