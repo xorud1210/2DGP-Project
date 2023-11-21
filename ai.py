33922cc96696eac2cd2715529b6785e02d5bf8cc
xorud1210 <rhxo1210@naver.com>
2023-11-21 19:52:25 +0900

    ai  공격 추가

--- ai.py
+++ ai.py
@@ -1,7 +1,9 @@
 import math
+import random
 
+import game_world
 from behavior_tree import BehaviorTree, Action, Sequence, Condition, Selector
-from pico2d import load_image
+from pico2d import load_image, get_time
 
 import game_framework
 import play_mode
@@ -23,7 +25,7 @@ FRAMES_PER_ACTION = 8
 
 class Ai:
     def __init__(self):
-        self.x, self.y = 800, 300
+        self.x, self.y = 1200, 300
         self.frame = 0
         self.action = 9
         self.x_dir = 0
@@ -36,9 +38,9 @@ class Ai:
         self.speed = 1.5
         self.attack_speed = 0.6
         self.role = 'knight'
-        self.stat = {'knight': {'power': 2, 'speed': 1.5, 'attack_speed': 0.6, 'weapon': Sword},
-                     'wizard': {'power': 4, 'speed': 1, 'attack_speed': 0.5, 'weapon': Orb},
-                     'archer': {'power': 6, 'speed': 1.2, 'attack_speed': 1.5, 'weapon': Arrow}}
+        self.stat = {'knight': {'power': 2, 'range': 2, 'speed': 1.5, 'attack_speed': 0.6, 'weapon': Sword},
+                     'wizard': {'power': 4, 'range': 12, 'speed': 1, 'attack_speed': 0.5, 'weapon': Orb},
+                     'archer': {'power': 6, 'range': 8, 'speed': 1.2, 'attack_speed': 1.5, 'weapon': Arrow}}
         self.image = {'knight': load_image('resource/player/knight.png'),
                       'wizard': load_image('resource/player/wizard.png'),
                       'archer': load_image('resource/player/archer.png')}
@@ -78,6 +80,12 @@ class Ai:
         self.action = self.sprite[self.role]['action'][state]
         self.max_frame = self.sprite[self.role]['max_frame'][state]
 
+    def thinking(self):
+        if get_time() - self.time <game_framework.frame_time * 1000:
+            return BehaviorTree.RUNNING
+        else:
+            return BehaviorTree.SUCCESS
+
     def set_dir(self, tx, ty):
         self.dir = math.atan2(ty - self.y, tx - self.x)
         self.x_dir = 1 if math.cos(self.dir) > 0 else -1
@@ -87,22 +95,63 @@ class Ai:
         distance2 = (x1 - x2) ** 2 + (y1 - y2) ** 2
         return distance2 < (PIXEL_PER_METER * r) ** 2
 
-    def move_to_ball(self, r=1):
+    def move_to_ball(self):
         self.state_change('walk')
         self.set_dir(play_mode.ball.x, play_mode.ball.y)
         self.speed = RUN_SPEED_PPS * self.stat[self.role]['speed']
         self.x += self.speed * math.cos(self.dir) * game_framework.frame_time
         self.y += self.speed * math.sin(self.dir) * game_framework.frame_time
 
-        if self.distance_less_than(play_mode.ball.x,play_mode.ball.y,self.x,self.y, r):
+        if self.distance_less_than(play_mode.ball.x,play_mode.ball.y,self.x,self.y, self.stat[self.role]['range']):
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.RUNNING
 
-    def build_behavior_tree(self):
-        a1 = Action('Move to ball',self.move_to_ball)
-        SEQ_move_to_ball = Sequence('Move to ball', a1)
+    def select_weapon(self):
+        roles = ['knight', 'wizard', 'archer']
+        self.role = roles[random.randint(0,2)]
 
-        root = SEQ_move_to_ball
+        return BehaviorTree.SUCCESS
+
+    def can_attack(self):
+        if self.distance_less_than(play_mode.ball.x,play_mode.ball.y,self.x,self.y, self.stat[self.role]['range']):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+
+    def cant_attack(self):
+        if self.distance_less_than(play_mode.ball.x,play_mode.ball.y,self.x,self.y, self.stat[self.role]['range']):
+            return BehaviorTree.FAIL
+        else:
+            return BehaviorTree.SUCCESS
+
+    def attack(self):
+        self.state_change('atk1')
+        weapon = self.stat[self.role]['weapon'](self.x + 40 * self.x_dir, self.y + 40 * self.y_dir, self.stat[self.role]['power'],
+                                                self.x_dir, self.y_dir)
+        if self.frame < self.max_frame - 0.05:
+            return BehaviorTree.RUNNING
+        else:
+            game_world.add_object(weapon)
+            self.time = get_time()
+            return BehaviorTree.SUCCESS
+
+
+    def build_behavior_tree(self):
+        a1 = Action('Weapon Select', self.select_weapon)
+        SEQ_select_weapon = Sequence('무기 고르기', a1)
+
+        c1 = Condition('공이 사거리 내에 있나?', self.can_attack)
+        a2 = Action('공격',self.attack)
+        a4 = Action('생각 중',self.thinking)
+        SEQ_attack = Sequence('공 때리기', c1, a2, a1, a4)
+
+        c2 = Condition('공이 사거리 보다 멀리 있나?', self.cant_attack)
+        a3 = Action('Move to ball',self.move_to_ball)
+        SEQ_move_to_ball = Sequence('Move to ball',  c2, a3)
+
+        root = SEL_attack_or_move = Selector('공격 혹은 이동', SEQ_attack, SEQ_move_to_ball)
+
+        # root = Selector('무기 선택 및 행동', SEQ_select_weapon, SEL_attack_or_move)
 
         self.bt = BehaviorTree(root)
