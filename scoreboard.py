b309815c83073039105406a063656f92d8289a44
xorud1210 <rhxo1210@naver.com>
2023-12-04 23:52:02 +0900

    게임 결과에 따른 모드 전환 추가

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,8 @@
 from pico2d import load_font, load_image, get_time
 
+import end_lose_mode
+import end_win_mode
+import game_framework
 import play_mode
 
 
@@ -7,11 +10,12 @@ class Scoreboard:
     def __init__(self):
         self.x = 790
         self.y = 1000
-        self.left_score = 0
-        self.right_score = 0
+        self.player_score = 0
+        self.ai_score = 0
+        self.target_score = 5
         self.font = load_font('ENCR10B.TTF', 50)
         self.image = load_image('resource/stadium/scoreboard.png')
-        self.time = get_time()
+        self.time = 0
         self.last_time = get_time()
         self.fever = False
 
@@ -21,12 +25,16 @@ class Scoreboard:
         self.last_time = cur_time
         if self.time > 50:
             self.fever = True
+        if self.player_score >= self.target_score:
+            game_framework.change_mode(end_win_mode)
+        elif self.ai_score >= self.target_score:
+            game_framework.change_mode(end_lose_mode)
         pass
 
     def draw(self):
         self.image.draw(self.x, self.y, 800, 600)
-        self.font.draw(self.x - 60,self.y, f'{self.left_score}', (255,255,255))
-        self.font.draw(self.x + 50,self.y, f'{self.right_score}', (255, 255, 255))
+        self.font.draw(self.x - 60, self.y, f'{self.player_score}', (255, 255, 255))
+        self.font.draw(self.x + 50, self.y, f'{self.ai_score}', (255, 255, 255))
         if self.fever:
             self.font.draw(self.x - 40, self.y - 60, f'{round(self.time, 2)}', (255, 0, 0))
         else:
